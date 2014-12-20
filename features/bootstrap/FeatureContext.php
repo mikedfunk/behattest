5dd3dffec5ff1d257a0b8c18a3a69c47e0b73bc3
Mike Funk <mike@mikefunk.com>
2014-12-19 18:30:19 -0800

    google test works

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,14 +1,24 @@
 <?php
+/**
+ * behat feature context
+ *
+ * @package BehatTest
+ * @license MIT License <http://opensource.org/licenses/mit-license.html>
+ */
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
+ *
+ * @author Michael Funk <mike@mikefunk.com>
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -19,5 +29,46 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given I am on the google homepage
+     */
+    public function iAmOnTheGoogleHomepage()
+    {
+        $this->iAmOnHomepage();
+    }
+
+    /**
+     * @When I enter the search term :arg1
+     */
+    public function iEnterTheSearchTerm($arg1)
+    {
+        $this->getSession()->wait(100);
+        // search input
+        $this->fillField('q', $arg1);
+        // you can also check for an element with javascript
+        // $this->getSession()
+        // ->wait(100, "document.getElementById('gbqfb').length");
+        $this->getSession()->wait(100);
+        // search button
+        $this->pressButton('gbqfb');
+    }
+
+    /**
+     * @Then I should see at least one result about :arg1
+     */
+    public function iShouldSeeAtLeastOneResultAbout($arg1)
+    {
+        $this->getSession()->wait(500);
+        $this->assertPageContainsText($arg1);
+    }
+
+    /**
+     * @Then I should be on the google search results page
+     */
+    public function iShouldBeOnTheGoogleSearchResultsPage()
+    {
+        $this->assertPageAddress('#q=pound+puppies');
     }
 }
--- features/googletest.feature
+++ features/googletest.feature
@@ -3,8 +3,9 @@ Feature: google search
     As a user
     I want to perform a search
 
+    @javascript
     Scenario: I get search results
         Given I am on the google homepage
         When I enter the search term "pound puppies"
-        Then I should see at least one result about "pound puppies"
-        And I should be on the google search results page
+        Then I should be on the google search results page
+        And I should see at least one result about "pound puppies"
